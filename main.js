a3b9a1c268c79e70d70bc71adf392ef2137476b4
Alexandre Ataide <acataide@gmail.com>
2015-12-13 17:41:54 -0200

    Looooooootssss of changes

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ $(function(){
       $(item).addClass("hidden");
     } else {
       // Give visible hexagons unique id
-      $(item).attr("id","slot:"+row+":"+col);
+      $(item).attr("id","slot_"+row+"_"+col);
       $(item).addClass("show");
       $(item).on("click", boardClick);
     } 
@@ -60,9 +60,7 @@ function boardClick (e) {
 }
 
 function turnClick (e) {
-  if (moment=="player") {
-    endTurn();
-  }
+  endTurn();
 }
 
 //End player turn
@@ -78,17 +76,90 @@ function endTurn() {
   } else {
     //keep growing
     $(".cancer").each(function(index, item){
-      $(item).text(parseInt($(item).text())+1);
+      var ATP = parseInt($(item).text()); 
+      $(item).text(ATP + 1);
+      //Big enough, will reproduce 
+      if (ATP >= 3) {
+        var src = getHex(item);
+        var dest = src.randNeighbor();
+        
+        //No Cell Present
+        if (getItem(dest).find(".cell").text() == 0) {
+          var dest_t = getItem(dest)
+                       .find(".tissue")
+                       .attr("class")
+                       .replace("tissue ","");
+
+          //Soft Tissue 
+          if (dest_t == "S_tissue" || dest_t == "H_tissue1" || dest_t == "VH_tissue2") {
+            divideCell(src, dest);
+          };
+          //Hard Tissue 
+          if (dest_t == "H_tissue") {
+            getItem(dest).find(".tissue").attr("class","tissue H_tissue1");
+          }
+          //Very Hard Tissue
+          if (dest_t == "VH_tissue") {
+            getItem(dest).find(".tissue").attr("class","tissue VH_tissue1");
+          }
+          if (dest_t == "VH_tissue1") {
+            getItem(dest).find(".tissue").attr("class","tissue VH_tissue2");
+          }
+        } else {
+      //Cell Present
+          var dest_cell = getItem(dest).find(".cell");
+          var dest_ATP = parseInt(dest_cell.text());
+          
+          var atk = dest_ATP - Math.floor(ATP/2);
+          if (atk > 0 ) {
+            dest_cell.text(atk);
+          } else {
+            dest_cell.text("");
+            dest_cell.attr("class","cell");
+          }
+        } 
+      }
     })
-  }
+  }  
 
   $(".stem_cell").each(function(index, item){
      var is_hard = $(item).parent().find(".VH_tissue").length;
      if (is_hard) {
+       $(item).text(parseInt($(item).text())+2);
+     } else {
        $(item).text(parseInt($(item).text())+1);
      }
   })
 }
+
+
+//Cell Mitosis
+function divideCell(src, des) {
+  var src_e = $("#slot_"+src.row+"_"+src.col);
+  var src_t = src_e.find(".tissue");
+  var ATP = Math.floor(parseInt(src_t.text()) / 2);
+  
+  var des_e = $("#slot_"+des.row+"_"+des.col);
+  var des_t = des_e.find(".tissue");
+  
+  src_t.text(ATP);
+  des_t.text(ATP);
+  des_t.attr("class", "tissue cancer");
+}
+
+//Get Hex from Element
+function getHex(item) {
+  var src = new Hex(parseInt($(item).parent().attr('row')),
+                    parseInt($(item).parent().attr('col')));
+  return src;
+}
+//Get Element from Hex
+function getItem(hex) {
+  var item = $("#slot_"+hex.row+"_"+hex.col);
+  return item;
+}
+
+
 
 // Updates Help Message
 function updateMsg(id) {
@@ -104,4 +175,17 @@ function fromCenter(row, col) {
   var dist = (Math.abs(x) + Math.abs(y) + Math.abs(z)) / 2;
   
   return dist;
+}
+
+
+//Debug Stuff
+function hilite(hex){
+  $("#slot_"+hex.row+"_"+hex.col).addClass("hilite");
+}
+
+function hiliteN(hex){
+  var n = hex.neighbors();
+  for (a in n) {
+    var item = $
+  }
 }
